e5925706c6a91a2d86f15ac0af5cc3a58a0cb4a3
skipole <42965323+bernie-skipole@users.noreply.github.com>
2016-11-25 22:17:53 +0000

    working on table1_button widget

---
+++ projectcode/widgets/links.py
@@ -0,0 +1,15 @@
+from ....skilift import FailPage, GoTo, ValidateError, ServerError
+
+def table1button(caller_ident, ident_list, submit_list, submit_dict, call_data, page_data, lang):
+    """Populates Table1_Button test page"""
+    # col 0 is the text string to place in the first column,
+    # col 1 is the get field contents of the button link
+    page_data['table1button', 'contents'] = [["Row 0 text", "test1"], ["Row 1 text", "test2"]]
+
+
+def table1button_jsontest(caller_ident, ident_list, submit_list, submit_dict, call_data, page_data, lang):
+    """Send JSON page after table button pressed"""
+    if call_data['table1button', 'contents'] == 'test1':
+        page_data['paratest','para_text'] = "First row button pressed"
+    if call_data['table1button', 'contents'] == 'test2':
+        page_data['paratest','para_text'] = "Second row button pressed"
