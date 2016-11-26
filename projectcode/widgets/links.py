e747c7f7ecb382cb80eb42abc93fde1913186f72
skipole <42965323+bernie-skipole@users.noreply.github.com>
2016-11-26 16:12:24 +0000

    working on button1 table

--- projectcode/widgets/links.py
+++ projectcode/widgets/links.py
@@ -5,6 +5,7 @@ def table1button(caller_ident, ident_list, submit_list, submit_dict, call_data, 
     # col 0 is the text string to place in the first column,
     # col 1 is the get field contents of the button link
     page_data['table1button', 'contents'] = [["Row 0 text", "test1"], ["Row 1 text", "test2"]]
+    page_data['table1button2', 'contents'] = [["Row 0 text", "test1"], ["Row 1 text", "test2"]]
 
 
 def table1button_jsontest(caller_ident, ident_list, submit_list, submit_dict, call_data, page_data, lang):
@@ -12,4 +13,11 @@ def table1button_jsontest(caller_ident, ident_list, submit_list, submit_dict, ca
     if call_data['table1button', 'contents'] == 'test1':
         page_data['paratest','para_text'] = "First row button pressed"
     if call_data['table1button', 'contents'] == 'test2':
+        page_data['paratest','para_text'] = "Second row button pressed"
+
+def table1button_htmltest(caller_ident, ident_list, submit_list, submit_dict, call_data, page_data, lang):
+    """Renew HML page after table button pressed"""
+    if call_data['table1button2', 'contents'] == 'test1':
+        page_data['paratest','para_text'] = "First row button pressed"
+    if call_data['table1button2', 'contents'] == 'test2':
         page_data['paratest','para_text'] = "Second row button pressed"
