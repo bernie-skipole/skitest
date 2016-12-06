2a161c6a6d1087b73b9e2dbb981d512e3ec8721a
skipole <42965323+bernie-skipole@users.noreply.github.com>
2016-12-06 11:11:53 +0000

    working on error messages

--- projectcode/widgets/error_messages.py
+++ projectcode/widgets/error_messages.py
@@ -5,5 +5,5 @@ def errordiv_test1(caller_ident, ident_list, submit_list, submit_dict, call_data
     raise FailPage(message = """This error message set in a Failpage exception by a submit data function.""", displaywidgetname="testerrordiv")
 
 def errordiv_test2(caller_ident, ident_list, submit_list, submit_dict, call_data, page_data, lang):
-    """Raises an error for errordiv, message set by SubmitData responder"""
+    """Raises an error for errordiv, with mo message, errordiv default message should be shown"""
     raise FailPage(displaywidgetname="testerrordiv")
