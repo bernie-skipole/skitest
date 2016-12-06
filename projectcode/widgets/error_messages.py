a3ff113bc80824320a93efea143c66b389571c78
skipole <42965323+bernie-skipole@users.noreply.github.com>
2016-12-06 13:47:23 +0000

    working on error messages

--- projectcode/widgets/error_messages.py
+++ projectcode/widgets/error_messages.py
@@ -5,5 +5,9 @@ def errordiv_test1(caller_ident, ident_list, submit_list, submit_dict, call_data
     raise FailPage(message = """This error message set in a Failpage exception by a submit data function.""", displaywidgetname="testerrordiv")
 
 def errordiv_test2(caller_ident, ident_list, submit_list, submit_dict, call_data, page_data, lang):
-    """Raises an error for errordiv, with mo message, errordiv default message should be shown"""
+    """Raises an error for errordiv, with no message, errordiv default message should be shown"""
     raise FailPage(displaywidgetname="testerrordiv")
+
+def errordiv_test5(caller_ident, ident_list, submit_list, submit_dict, call_data, page_data, lang):
+    """Clears an error for errordiv"""
+    page_data['testerrordiv', 'clear_error'] = True
