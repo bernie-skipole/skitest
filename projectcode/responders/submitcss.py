ecc9f147d1eb13317d69bfbe6ce4a314fce51ec7
skipole <42965323+bernie-skipole@users.noreply.github.com>
2017-05-18 22:07:03 +0100

    added submitcss test

--- projectcode/responders/submitcss.py
+++ projectcode/responders/submitcss.py
@@ -1,6 +1,18 @@
 from ....skilift import FailPage, GoTo, ValidateError, ServerError
 
 def test_blue(caller_ident, ident_list, submit_list, submit_dict, call_data, page_data, lang):
-    """Tests submitcss by returning a css style makeblue"""
-    style = { '.makeblue': [['background-color', 'blue'], ['color', 'white']] }
-    return style
+    """Tests submitcss by returning a css style makeblue, white text on a blue background"""
+    return { '.makeblue': [['background-color', 'blue'],
+                           ['color', 'white'],
+                           ['margin', '10px'],
+                           ['padding', '10px']
+                           ] }
+
+
+def test_invblue(caller_ident, ident_list, submit_list, submit_dict, call_data, page_data, lang):
+    """Tests submitcss by returning a css style makeblue, blue text on a white background"""
+    return { '.makeblue': [['background-color', 'white'],
+                           ['color', 'blue'],
+                           ['margin', '10px'],
+                           ['padding', '10px']
+                           ] }
