3cc1082dad4c761d56a3a88069a3e8f5ed6bb885
skipole <42965323+bernie-skipole@users.noreply.github.com>
2017-06-08 17:00:45 +0100

    errorpara tests done

--- projectcode/widgets/error_messages.py
+++ projectcode/widgets/error_messages.py
@@ -19,3 +19,25 @@ def errordiv_test6(caller_ident, ident_list, submit_list, submit_dict, call_data
         page_data['resulttext','para_text'] = "Text received : %s." % call_data['textinput','input_text']
     else:
         page_data['resulttext','para_text'] = "No text received."
+
+
+def errorpara_test1(caller_ident, ident_list, submit_list, submit_dict, call_data, page_data, lang):
+    """Raises an error for errorpara"""
+    raise FailPage(message = """This error message set in a Failpage exception by a submit data function.""", displaywidgetname="testerrorpara")
+
+def errorpara_test2(caller_ident, ident_list, submit_list, submit_dict, call_data, page_data, lang):
+    """Raises an error for errorpara, with no message, errorpara default message should be shown"""
+    raise FailPage(displaywidgetname="testerrorpara")
+
+def errorpara_test5(caller_ident, ident_list, submit_list, submit_dict, call_data, page_data, lang):
+    """Clears an error for errorpara"""
+    page_data['testerrorpara', 'clear_error'] = True
+
+
+def errorpara_test6(caller_ident, ident_list, submit_list, submit_dict, call_data, page_data, lang):
+    """Puts received string in resulttext,para_text"""
+    if call_data['textinput','input_text']:
+        page_data['resulttext','para_text'] = "Text received : %s." % call_data['textinput','input_text']
+    else:
+        page_data['resulttext','para_text'] = "No text received."
+
