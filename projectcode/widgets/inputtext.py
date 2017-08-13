b21ae7fb1d84f63cb4a75385f66b40e6ebb9399a
skipole <42965323+bernie-skipole@users.noreply.github.com>
2017-08-13 11:46:00 +0100

    working on tests

--- projectcode/widgets/inputtext.py
+++ projectcode/widgets/inputtext.py
@@ -6,5 +6,10 @@ def textinput1_test1(caller_ident, ident_list, submit_list, submit_dict, call_da
     if ('textinput1_test1','input_text') in call_data:
         page_data['result','para_text'] = call_data['textinput1_test1','input_text']
 
+def textinput2_test1(caller_ident, ident_list, submit_list, submit_dict, call_data, page_data, lang):
+    """Takes input from textinput2 widget"""
+    if ('textinput2_test1','input_text') in call_data:
+        page_data['result','para_text'] = call_data['textinput2_test1','input_text']
+
 
 
