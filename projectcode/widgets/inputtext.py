9947d8dc3f98d32fbfa4454cfcf300f2a8eab366
skipole <42965323+bernie-skipole@users.noreply.github.com>
2017-09-04 14:59:20 +0100

    started twoinputssubmit1 form

--- projectcode/widgets/inputtext.py
+++ projectcode/widgets/inputtext.py
@@ -65,3 +65,24 @@ def submittextinput3_test6(caller_ident, ident_list, submit_list, submit_dict, c
         page_data['submittextinput3_test1','para_text'] = call_data['submittextinput3_test6','input_text']
         page_data['submittextinput3_test1','show_para1'] = True
 
+
+def twoinputssubmit1_test1(caller_ident, ident_list, submit_list, submit_dict, call_data, page_data, lang):
+    """Takes input from TwoInputsSubmit1 widget"""
+    if ('twoinputssubmit1_test1','input_text1') in call_data:
+        text1 = call_data['twoinputssubmit1_test1','input_text1']
+    else:
+        text1 = ''
+    if ('twoinputssubmit1_test1','input_text2') in call_data:
+        text2 = call_data['twoinputssubmit1_test1','input_text2']
+    else:
+        text2 = ''
+    if text1 and text2:
+        page_data['result','para_text'] = text1 + " , " + text2
+    elif text1:
+        page_data['result','para_text'] = text1
+    elif text2:
+        page_data['result','para_text'] = text2
+    else:
+        page_data['result','para_text'] = "Nothing received"
+
+
