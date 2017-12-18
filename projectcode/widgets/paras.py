e291cd601c93871e6398d7c226cc0ba860eef8b8
skipole <42965323+bernie-skipole@users.noreply.github.com>
2017-12-18 23:34:48 +0000

    working on tagblock drag and drop

--- projectcode/widgets/paras.py
+++ projectcode/widgets/paras.py
@@ -3,7 +3,9 @@ from ... import FailPage, GoTo, ValidateError, ServerError
 def tagblock_test1(caller_ident, ident_list, submit_list, submit_dict, call_data, page_data, lang):
     """Changes tagblock1 to colour of drop in"""
     destination_color = call_data['tagblock2', 'drag']
-    page_data['tagblock1', 'widget_style'] = "color:" + destination_color + ";"
+    page_data['tagblock1', 'widget_style'] = "color:" + destination_color + ";margin-left:15%;width:10%;margin-right:auto;background-color:grey;text-align: center;"
+
+
 
 
 
