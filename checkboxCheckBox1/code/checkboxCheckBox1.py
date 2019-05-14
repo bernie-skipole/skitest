fc9a8ee1658499896f4775f35d5d398f50f85f49
skipole <42965323+bernie-skipole@users.noreply.github.com>
2019-05-14 14:11:30 +0100

    added cache true the theme4

--- checkboxCheckBox1/code/checkboxCheckBox1.py
+++ checkboxCheckBox1/code/checkboxCheckBox1.py
@@ -44,6 +44,8 @@ def start_call(called_ident, skicall):
 
 def submit_data(skicall):
     "This function is called when a Responder wishes to submit data for processing in some manner"
+    if skicall.submit_list[0] == 'test1':
+        _test1(skicall)
     return
 
 
@@ -51,6 +53,27 @@ def end_call(page_ident, page_type, skicall):
     """This function is called at the end of a call prior to filling the returned page with skicall.page_data,
        it can also return an optional session cookie string."""
     return
+
+
+def _test1(skicall):
+    """Tests CheckBox1 when submitted in a form"""
+    if skicall.call_data['check1','checkbox'] == 'check1':
+        skicall.page_data['check1result', 'para_text'] = "Form submission result: The checkbox is TICKED."
+        skicall.page_data['check1', 'checked'] = True
+    else:
+       skicall.page_data['check1result', 'para_text'] = "Form submission result: The checkbox is NOT TICKED."
+       skicall.page_data['check1', 'checked'] = False
+
+
+def _test2(skicall):
+    """Raises an error in CheckBox1"""
+    raise FailPage(message="Test error raised", widget='check1')
+
+
+def _test3(skicall):
+    """Raises an error in CheckBox1 by JSON"""
+    raise FailPage(message="Test error raised", widget='check1')
+
 
 
 ##############################################################################
@@ -149,7 +172,7 @@ if __name__ == "__main__":
 
     # serve the application
     host = "127.0.0.1"
-    port = 8000
+    port = 8001
 
     httpd = make_server(host, port, application)
     print("Serving %s on port %s. Call http://localhost:%s/skiadmin to edit." % (PROJECT, port, port))
