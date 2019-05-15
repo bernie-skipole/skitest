ac24ef0513d82aa5e6575f2b3bf0ef36d917077e
skipole <42965323+bernie-skipole@users.noreply.github.com>
2019-05-15 09:41:59 +0100

    adding checkbox2 project

--- skitest/code/skitest_packages/widgets/checkbox.py
+++ skitest/code/skitest_packages/widgets/checkbox.py
@@ -1,33 +1,5 @@
 from skipole import FailPage, GoTo, ValidateError, ServerError
 
-def check1(skicall):
-    """Tests CheckBox1 when submitted in a form"""
-    if skicall.call_data['check1','checkbox'] == 'check1':
-        skicall.page_data['check1result', 'para_text'] = "Form submission result: The checkbox is TICKED."
-        skicall.page_data['check1', 'checked'] = True
-    else:
-       skicall.page_data['check1result', 'para_text'] = "Form submission result: The checkbox is NOT TICKED."
-       skicall.page_data['check1', 'checked'] = False
-
-
-def check2(skicall):
-    """Raises an error in CheckBox1"""
-    raise FailPage(message="Test error raised", widget='check1')
-
-
-def check5(skicall):
-    """Raises an error in CheckBox1 by JSON"""
-    raise FailPage(message="Test error raised", widget='check1')
-
-
-def check10(skicall):
-    """Tests CheckBox2 when submitted in a form"""
-    if skicall.call_data['check10','checkbox'] == 'check10':
-        skicall.page_data['check10result', 'para_text'] = "Form submission result: The checkbox is TICKED."
-        skicall.page_data['check10', 'checked'] = True
-    else:
-       skicall.page_data['check10result', 'para_text'] = "Form submission result: The checkbox is NOT TICKED."
-       skicall.page_data['check10', 'checked'] = False
 
 
 
