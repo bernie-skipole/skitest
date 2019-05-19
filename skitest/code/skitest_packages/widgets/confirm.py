3524d5db56fe063a984fa9d54e8f65d89fbfc8fa
skipole <42965323+bernie-skipole@users.noreply.github.com>
2019-05-19 20:23:04 +0100

    confirmbox1 done

--- skitest/code/skitest_packages/widgets/confirm.py
+++ skitest/code/skitest_packages/widgets/confirm.py
@@ -18,24 +18,6 @@ def alertclear1_test4(skicall):
     raise FailPage(message="This raises an error and sends JSON file", widget='alertclear1')
 
 
-def confirmbox1_test1(skicall):
-    """Displays confirmbox1 by HTML"""
-    skicall.page_data['confirmbox1', 'hide'] = False
-    skicall.page_data['confirmbox1', 'para_text'] = """Text sent from a submit_data function, this refreshes the full page, and sets the confirmbox1 widget 'hide' field to False, which therefore should display this message."""
-
-def confirmbox1_test2(skicall):
-    """Displays confirmbox1 by JSON"""
-    skicall.page_data['confirmbox1', 'hide'] = False
-    skicall.page_data['confirmbox1', 'para_text'] = """Text sent from a submit_data function, this sends a JSON file with the confirmbox1 widget 'hide' field set to False, which therefore should display this message."""
-
-def confirmbox1_test3(skicall):
-    """Hide confirmbox1 with json call"""
-    skicall.page_data['confirmbox1', 'hide'] = True
-
-def confirmbox1_test4(skicall):
-    """Hide confirmbox1 with html call"""
-    skicall.page_data['confirmbox1', 'hide'] = True
-
 def confirmbox2_test1(skicall):
     """Displays confirmbox2 by HTML"""
     skicall.page_data['confirmbox2', 'hide'] = False
