a9d59280624c757c39881e3cf3371c630a6f5e9b
skipole <42965323+bernie-skipole@users.noreply.github.com>
2019-05-27 11:35:15 +0100

    added test for alertclear1

--- skitest/code/skitest_packages/widgets/confirm.py
+++ skitest/code/skitest_packages/widgets/confirm.py
@@ -1,22 +1,5 @@
 from skipole import FailPage, GoTo, ValidateError, ServerError
 
-def alertclear1_test1(skicall):
-    """Displays alertclear1"""
-    skicall.page_data['alertclear1', 'hide'] = False
-    skicall.page_data['alertclear1', 'para_text'] = """Text sent from a submit_data function, this refreshes the full page, and sets the alertclear1 widget 'hide' field to False, which therefore should display this message."""
-
-def alertclear1_test2(skicall):
-    """Displays alertclear1"""
-    skicall.page_data['alertclear1', 'hide'] = False
-    skicall.page_data['alertclear1', 'para_text'] = """Text sent from a submit_data function, which returns a JSON page, and sets the alertclear1 widget 'hide' field to False, which therefore should display this message."""
-
-
-def alertclear1_test3(skicall):
-    raise FailPage(message="This raises an error and refreshes the page", widget='alertclear1')
-
-def alertclear1_test4(skicall):
-    raise FailPage(message="This raises an error and sends JSON file", widget='alertclear1')
-
 
 def confirmbox2_test1(skicall):
     """Displays confirmbox2 by HTML"""
