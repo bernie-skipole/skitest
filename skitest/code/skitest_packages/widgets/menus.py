6ef56b92dcce9cb6886f1ff4f905e62bf6e6091a
skipole <42965323+bernie-skipole@users.noreply.github.com>
2019-05-30 17:33:39 +0100

    clearing out old widget menu system

--- skitest/code/skitest_packages/widgets/menus.py
+++ skitest/code/skitest_packages/widgets/menus.py
@@ -6,25 +6,17 @@ def modules_menu(skicall):
     # each tuple in the list below is button text, link ident
 
     butt_list = [
-                 ('debug_tools', 3012),
-                 ('dropdown', 3013),
                  ('error_messages', 'error_messages'),
                  ('footers', 3014),
                  ('headers', 3015),
                  ('info', 'info'),
                  ('input_forms', 3009),
-                 ('inputtables', 3016),
                  ('inputtext', 3010),
                  ('links', 'links'),
                  ('lists', 'lists'),
                  ('logins', 'logins'),
                  ('paras', 'paras'),
-                 ('radio', 3017),
-                 ('svgbasics', 3019),
-                 ('svgmeters', 3018),
-                 ('tables', 3020),
-                 ('textarea', 3021),
-                 ('upload', 3022) ]
+                 ('svgmeters', 3018) ]
 
 
     skicall.page_data['modbuttons', 'multiplier'] = len(butt_list)
