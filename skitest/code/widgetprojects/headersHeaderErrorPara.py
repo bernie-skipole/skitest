942fe241a31940e45e49e50e9f9662be364f82a9
bernie-skipole <bernie@skipole.co.uk>
2019-11-20 20:22:41 +0000

    HeaderErrorPara added

---
+++ skitest/code/widgetprojects/headersHeaderErrorPara.py
@@ -0,0 +1,35 @@
+
+from skipole import WSGIApplication, FailPage, GoTo, ValidateError, ServerError
+
+def start_call(called_ident, skicall):
+    "When a call is initially received this function is called."
+    return called_ident
+
+
+def submit_data(skicall):
+    "This function is called when a Responder wishes to submit data for processing in some manner"
+    if skicall.submit_list[0] == 'test1':
+        _test1(skicall)
+    return
+
+
+def end_call(page_ident, page_type, skicall):
+    """This function is called at the end of a call prior to filling the returned page with skicall.page_data,
+       it can also return an optional session cookie string."""
+    return
+
+
+def _test1(skicall):
+    """Test 1"""
+    raise FailPage()
+
+
+def makeapp(projectfiles, **proj_data):
+    """returns the application"""
+    return WSGIApplication(project='headersHeaderErrorPara',
+                           projectfiles=projectfiles,
+                           proj_data=proj_data,
+                           start_call=start_call,
+                           submit_data=submit_data,
+                           end_call=end_call,
+                           url="/")
